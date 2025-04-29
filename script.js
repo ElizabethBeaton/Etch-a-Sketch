6e102693ec0163ce36f6868582215d3b7182d302
ElizabethBeaton <Elizabethbeaton23@outlook.com>
2025-04-29 14:59:00 +0100

    added 3 digit padding to numbers and hover effect

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const container = document.getElementById("grid-container")
+const container = document.getElementById("grid-container");
 
-for (let i = 0; i <= 16; i++ ){
-    const div = document.createElement("div");
-    div.className = "grid-item";
-    div.textContent = `Div ${i}`;
-    div.style.backgroundColor = "pink"
-    container.appendChild(div);
+for (let i = 1; i <= 256; i++) {  // 256 times, create a div. In css 640x640 makes it show row of 8
+  const div = document.createElement("div");
+  div.className = "grid-item";
+  div.textContent = `Div ${String(i).padStart(3, "0")}`;   // added more digit placeholders to make sure grid is even and aligned
+  div.style.margin = "2px"
+  div.style.padding = "10px"
+
+  // Make every 16th red
+  div.style.backgroundColor = i % 16 === 0 ? "red" : "pink"; // if a multiple of 16, make red
+
+    // turn div boxed green when hovered
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "green"
+})
+
+  container.appendChild(div);
 }
-
-
